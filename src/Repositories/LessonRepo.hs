a232443ae6a4af91a2d7ee9743684439ac9b82f9
claraneves23 <mariaclarapn14@gmail.com>
2026-06-04 21:36:46 -0300

    feat(backend): adiciona update e delete no LessonRepo

--- src/Repositories/LessonRepo.hs
+++ src/Repositories/LessonRepo.hs
@@ -4,15 +4,21 @@ module Repositories.LessonRepo
   , create
   , update
   , delete
+  , deleteCascade
+  , nextOrderIdx
+  , resequence
   ) where
 
 import Control.Monad.IO.Class (MonadIO)
 import Database.Persist
-  ( Entity
-  , SelectOpt (Asc)
+  ( Entity (..)
+  , SelectOpt (Asc, Desc, LimitTo)
+  , deleteWhere
   , insert
   , replace
   , selectList
+  , (<-.)
+  , (=.)
   , (==.)
   )
 import qualified Database.Persist as P
@@ -33,3 +39,27 @@ update = replace
 
 delete :: MonadIO m => LessonId -> SqlPersistT m ()
 delete = P.delete
+
+-- | Remove a lição junto de tudo que depende dela (exercícios e progresso),
+-- evitando registros órfãos e violação de FK.
+deleteCascade :: MonadIO m => LessonId -> SqlPersistT m ()
+deleteCascade lid = do
+  exs <- selectList [ExerciseLessonId ==. lid] []
+  deleteWhere [ExerciseAttemptExerciseId <-. map entityKey exs]
+  deleteWhere [ExerciseLessonId ==. lid]
+  deleteWhere [ProgressLessonId ==. lid]
+  P.delete lid
+
+-- | Próxima posição livre na sequência de lições de um módulo.
+nextOrderIdx :: MonadIO m => ModuleId -> SqlPersistT m Int
+nextOrderIdx mid = do
+  ls <- selectList [LessonModuleId ==. mid] [Desc LessonOrderIdx, LimitTo 1]
+  pure $ case ls of
+    (Entity _ l : _) -> lessonOrderIdx l + 1
+    []               -> 1
+
+-- | Renumera as lições do módulo para 1..n por ordem atual, fechando lacunas.
+resequence :: MonadIO m => ModuleId -> SqlPersistT m ()
+resequence mid = do
+  ls <- selectList [LessonModuleId ==. mid] [Asc LessonOrderIdx]
+  mapM_ (\(i, Entity lid _) -> P.update lid [LessonOrderIdx =. i]) (zip [1 ..] ls)
