4634de1c1bbf1b609cb645098ce3bfb66fedae95
claraneves23 <mariaclarapn14@gmail.com>
2026-06-04 21:36:44 -0300

    feat(api): adiciona tipos de profile, dashboard e exercise response

--- src/API/Types.hs
+++ src/API/Types.hs
@@ -18,14 +18,26 @@ module API.Types
   , ExerciseRequest (..)
   , ExerciseResponse (..)
   , toExerciseResponse
+  , toExerciseResponseFull
   , SubmitExerciseRequest (..)
   , SubmitExerciseResponse (..)
+  , ExerciseResponseEntry (..)
+  , toExerciseResponseEntry
     -- Progress
   , ProgressEntry (..)
   , toProgressEntry
   , CompleteLessonRequest (..)
     -- Roadmap
   , RoadmapItemResponse (..)
+    -- Profile
+  , ProfileResponse (..)
+  , ProfileRequest (..)
+  , toProfileResponse
+    -- Dashboard
+  , DashboardResponse (..)
+  , DashboardModuleStat (..)
+  , DashboardActivity (..)
+  , DashboardStudent (..)
     -- Diagnostic
   , DiagnosticQuestionResponse (..)
   , toDiagnosticQuestionResponse
@@ -52,10 +64,15 @@ import GHC.Generics (Generic)
 -- Auth ----------------------------------------------------------------
 
 data RegisterRequest = RegisterRequest
-  { rrEmail    :: Text
-  , rrPassword :: Text
-  , rrName     :: Text
-  , rrRole     :: Role
+  { rrEmail      :: Text
+  , rrPassword   :: Text
+  , rrName       :: Text
+  , rrRole       :: Role
+  , rrCourse     :: Maybe Text
+  , rrEnrollment :: Maybe Text
+  , rrSemester   :: Maybe Text
+  , rrShift      :: Maybe Text
+  , rrDiscipline :: Maybe Text
   } deriving (Generic, Show)
 instance FromJSON RegisterRequest
 instance ToJSON RegisterRequest
@@ -168,19 +185,31 @@ data ExerciseResponse = ExerciseResponse
   , ersPrompt      :: Text
   , ersPayload     :: Value
   , ersOrderIdx    :: Int
-  -- intencionalmente sem answer/explanation no GET (esses só vêm pós-submit)
+  -- answer/explanation só vêm preenchidos para professor (consulta/edição) ou pós-submit
+  , ersAnswer      :: Maybe Value
+  , ersExplanation :: Maybe Text
   } deriving (Generic, Show)
 instance FromJSON ExerciseResponse
 instance ToJSON ExerciseResponse
 
+-- | Visão básica (aluno): sem gabarito nem explicação.
 toExerciseResponse :: Entity Exercise -> ExerciseResponse
 toExerciseResponse (Entity eid e) = ExerciseResponse
-  { ersId       = fromSqlKey eid
-  , ersLessonId = fromSqlKey (exerciseLessonId e)
-  , ersKind     = exerciseKind e
-  , ersPrompt   = exercisePrompt e
-  , ersPayload  = decodeStored (exercisePayload e)
-  , ersOrderIdx = exerciseOrderIdx e
+  { ersId          = fromSqlKey eid
+  , ersLessonId    = fromSqlKey (exerciseLessonId e)
+  , ersKind        = exerciseKind e
+  , ersPrompt      = exercisePrompt e
+  , ersPayload     = decodeStored (exercisePayload e)
+  , ersOrderIdx    = exerciseOrderIdx e
+  , ersAnswer      = Nothing
+  , ersExplanation = Nothing
+  }
+
+-- | Visão completa (professor): inclui gabarito e explicação.
+toExerciseResponseFull :: Entity Exercise -> ExerciseResponse
+toExerciseResponseFull (Entity eid e) = (toExerciseResponse (Entity eid e))
+  { ersAnswer      = Just (decodeStored (exerciseAnswer e))
+  , ersExplanation = Just (exerciseExplanation e)
   }
 
 -- | Decodifica JSON armazenado como Text. Devolve Null se inválido.
@@ -201,6 +230,22 @@ data SubmitExerciseResponse = SubmitExerciseResponse
   } deriving (Generic, Show)
 instance FromJSON SubmitExerciseResponse
 instance ToJSON SubmitExerciseResponse
+
+-- | Resposta já enviada por um aluno (para bloquear/repintar no front).
+data ExerciseResponseEntry = ExerciseResponseEntry
+  { rseExerciseId :: Int64
+  , rseAnswer     :: Value
+  , rseCorrect    :: Bool
+  } deriving (Generic, Show)
+instance FromJSON ExerciseResponseEntry
+instance ToJSON ExerciseResponseEntry
+
+toExerciseResponseEntry :: Entity ExerciseAttempt -> ExerciseResponseEntry
+toExerciseResponseEntry (Entity _ r) = ExerciseResponseEntry
+  { rseExerciseId = fromSqlKey (exerciseAttemptExerciseId r)
+  , rseAnswer     = decodeStored (exerciseAttemptAnswer r)
+  , rseCorrect    = exerciseAttemptCorrect r
+  }
 
 -- Progress ------------------------------------------------------------
 
@@ -240,6 +285,92 @@ data RoadmapItemResponse = RoadmapItemResponse
   } deriving (Generic, Show)
 instance FromJSON RoadmapItemResponse
 instance ToJSON RoadmapItemResponse
+
+-- Profile -------------------------------------------------------------
+
+data ProfileResponse = ProfileResponse
+  { prId         :: Int64
+  , prEmail      :: Text
+  , prName       :: Text
+  , prRole       :: Role
+  , prCourse     :: Maybe Text
+  , prEnrollment :: Maybe Text
+  , prSemester   :: Maybe Text
+  , prShift      :: Maybe Text
+  , prDiscipline :: Maybe Text
+  } deriving (Generic, Show)
+instance FromJSON ProfileResponse
+instance ToJSON ProfileResponse
+
+data ProfileRequest = ProfileRequest
+  { puName       :: Text
+  , puCourse     :: Maybe Text
+  , puEnrollment :: Maybe Text
+  , puSemester   :: Maybe Text
+  , puShift      :: Maybe Text
+  , puDiscipline :: Maybe Text
+  } deriving (Generic, Show)
+instance FromJSON ProfileRequest
+instance ToJSON ProfileRequest
+
+toProfileResponse :: Entity User -> ProfileResponse
+toProfileResponse (Entity uid u) = ProfileResponse
+  { prId         = fromSqlKey uid
+  , prEmail      = userEmail u
+  , prName       = userName u
+  , prRole       = userRole u
+  , prCourse     = userCourse u
+  , prEnrollment = userEnrollment u
+  , prSemester   = userSemester u
+  , prShift      = userShift u
+  , prDiscipline = userDiscipline u
+  }
+
+-- Dashboard -----------------------------------------------------------
+
+data DashboardModuleStat = DashboardModuleStat
+  { dmsModuleId       :: Int64
+  , dmsTitle          :: Text
+  , dmsCompletionRate :: Double  -- % de alunos que concluíram o módulo
+  , dmsAccuracyRate   :: Double  -- % de acertos nas tentativas dos exercícios do módulo
+  , dmsAttempts       :: Int
+  } deriving (Generic, Show)
+instance FromJSON DashboardModuleStat
+instance ToJSON DashboardModuleStat
+
+data DashboardActivity = DashboardActivity
+  { dacUserName :: Text
+  , dacPrompt   :: Text
+  , dacCorrect  :: Bool
+  , dacAt       :: UTCTime
+  } deriving (Generic, Show)
+instance FromJSON DashboardActivity
+instance ToJSON DashboardActivity
+
+data DashboardStudent = DashboardStudent
+  { dstName      :: Text
+  , dstEmail     :: Text
+  , dstCreatedAt :: UTCTime
+  } deriving (Generic, Show)
+instance FromJSON DashboardStudent
+instance ToJSON DashboardStudent
+
+data DashboardResponse = DashboardResponse
+  { dbTotalStudents       :: Int
+  , dbTotalModules        :: Int
+  , dbTotalLessons        :: Int
+  , dbTotalExercises      :: Int
+  , dbAvgProgress         :: Double  -- progresso médio dos alunos (0..100)
+  , dbAccuracyRate        :: Double  -- taxa de acertos global (0..100)
+  , dbTotalAttempts       :: Int
+  , dbActiveStudents      :: Int     -- ativos nos últimos 14 dias
+  , dbLowProgressStudents :: Int     -- progresso < 30%
+  , dbModules             :: [DashboardModuleStat]
+  , dbRecentActivity      :: [DashboardActivity]
+  , dbNewStudents         :: [DashboardStudent]
+  } deriving (Generic, Show)
+instance FromJSON DashboardResponse
+instance ToJSON DashboardResponse
 
 -- Diagnostic ----------------------------------------------------------
 
