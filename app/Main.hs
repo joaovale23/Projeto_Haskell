57d6e2c5ee97a805639be5e14fbb7ccc21a03080
João Vitor Vale <joaovitorvalec@gmail.com>
2026-06-10 18:17:04 -0300

    fiz: logs console

--- app/Main.hs
+++ app/Main.hs
@@ -5,12 +5,17 @@ import Config.AppConfig (AppConfig (..), loadConfig)
 import Database.Pool (makePool, runMigrations)
 import Network.Wai.Handler.Warp (run)
 import Server (app)
+import System.IO (BufferMode (LineBuffering), hSetBuffering, stderr, stdout)
 
 main :: IO ()
 main = do
+  -- Em container, stdout/stderr sao pipes e o GHC usa block-buffering por
+  -- padrao, o que faz os logs nao aparecerem no Render. Forca line-buffering.
+  hSetBuffering stdout LineBuffering
+  hSetBuffering stderr LineBuffering
   cfg  <- loadConfig
   putStrLn ("Conectando ao banco em " <> show (acDatabaseUrl cfg))
-  pool <- makePool (acDatabaseUrl cfg) 10
+  pool <- makePool (acDatabaseUrl cfg) 3
   putStrLn "Rodando migracoes..."
   runMigrations pool
   let env = Env { envDbPool = pool, envConfig = cfg }
